5cb2ebb89d928b08a18436d73904550f588e6584
[Arjun Naik] <[arjunnaik333@gmail.com]>
2023-07-08 14:12:50 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState,useEffect } from "react";
 
 function App() {
+
+  const [pictures,setPictures]=useState([])
+  const [search,setSearch]=useState("")
+  
+  const url = "https://reqres.in/api/users?page=2";
+
+  const getData = async () => {
+    const allData = await fetch("https://reqres.in/api/users?page=2", {
+      method: "Get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const resp = await allData.json();
+setPictures(resp.data)
+    console.log(resp);
+    
+  };
+console.log(pictures)
+  useEffect(()=>{
+  getData()
+},[search])
+
+ const handleSearch=()=>{
+  console.log(
+  pictures.filter((i)=>i.first_name===search)
+  )
+  setPictures( pictures.filter((i)=>i.first_name.toLowerCase()===search.toLowerCase()))
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search_bar">
+        <input
+          className="input_data"
+          onChange={(e)=>{setSearch(e.target.value)}}
+        />
+        <button 
+        onClick={handleSearch}>Search</button>
+      </div>
+      <div>
+       {pictures.map((i)=>(
+          <div className="emp_list">
+            <img id="7" src={i.avatar} />
+            <h1>{i.first_name}</h1>
+          </div>
+       
+  
+       ))
+       }
+           </div>
     </div>
   );
 }
